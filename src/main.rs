80b24a6675b45037f337bd35c17b8d6d6526ff14
aromeronavia <alberto@neurelo.com>
2022-12-22 12:49:52 -0600

    Create App Struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 mod entities;
 mod client;
 
-use crossterm::{terminal::{enable_raw_mode, EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen}, execute, event::{EnableMouseCapture, DisableMouseCapture}};
-use tokio;
+use crossterm::{terminal::{enable_raw_mode, EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen}, execute, event::{EnableMouseCapture, DisableMouseCapture, self, Event, KeyCode}};
+use tokio::{self, time::Instant};
 use dotenv;
-use std::{io::{self, Stdout}, thread, time::Duration};
-use tui::{backend::CrosstermBackend, Terminal, widgets::{Block, Borders, List, ListItem}};
+use std::{io::{self, Stdout}, time::Duration};
+use tui::{backend::CrosstermBackend, Terminal, widgets::{Block, Borders, List, ListItem, ListState}, style::{Style, Modifier}};
 
 use entities::Playlist;
 use client::SpotifyClient;
+
+struct App {
+    playlists: Vec<Playlist>,
+    playlists_ui_state: ListState,
+    current_playlist: Option<Playlist>,
+}
+
+impl App {
+    fn new(playlists: Vec<Playlist>) -> Self {
+        let mut playlists_ui_state = ListState::default();
+        playlists_ui_state.select(Some(0));
+
+        Self {
+            playlists,
+            playlists_ui_state,
+            current_playlist: None,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
@@ -21,18 +40,49 @@ async fn main() -> Result<(), io::Error> {
     let backend: CrosstermBackend<Stdout> = CrosstermBackend::new(stdout);
     let mut terminal: Terminal<CrosstermBackend<Stdout>> = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        let size = f.size();
-        let list_items: Vec<ListItem> = playlists.iter().map(|playlist: &Playlist| 
-            ListItem::new(playlist.name.as_str())
-        ).collect::<Vec<ListItem>>();
-        let block = Block::default().title("Playlists").borders(Borders::ALL);
-        let list: List = List::new(list_items).block(block);
+    let mut last_tick = Instant::now();
 
-        f.render_widget(list, size);
-    })?;
+    let mut app = App::new(playlists);
 
-    thread::sleep(Duration::from_millis(5000));
+    loop {
+        terminal.draw(|f| {
+            let size = f.size();
+            let list_items: Vec<ListItem> = app.playlists.iter().map(
+                |playlist: &Playlist| ListItem::new(playlist.name.as_str()
+            )).collect::<Vec<ListItem>>();
+
+            let block = Block::default().title("Playlists").borders(Borders::ALL);
+            let list: List = List::new(list_items)
+                .block(block)
+                .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+                .highlight_symbol(">>");
+
+            f.render_stateful_widget(list, size, &mut app.playlists_ui_state);
+        })?;
+
+        let tick_rate = Duration::from_millis(50);
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        if event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => {
+                        break;
+                    }
+                    KeyCode::Char('c') if key.modifiers.contains(event::KeyModifiers::CONTROL) => {
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now();
+        }
+    }
 
     // restore terminal
     disable_raw_mode()?;
