22f390ceefe74a86d6814f33804e9cab18e436fb
aromeronavia <alberto@neurelo.com>
2022-12-21 19:00:00 -0600

    Get my playlists without the proper parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::sync::Arc;
-
-use rspotify::{model::{AlbumId, FullAlbum}, prelude::*, ClientCredsSpotify, Credentials};
+use rspotify::{model::{AlbumId, FullAlbum}, prelude::*, ClientCredsSpotify, Credentials, AuthCodeSpotify, OAuth, scopes};
 use tokio;
 use dotenv;
 use anyhow::Result;
@@ -22,22 +20,36 @@ async fn get_album() {
     let album = spotify_client.get_album(album_uri).await.unwrap();
 
     println!("Response: {:?}", album.songs);
+    println!("Get my playlists: {:?}", spotify_client.get_my_playlists().await)
 }
 
 pub struct SpotifyClient {
-    client: ClientCredsSpotify,
+    client: AuthCodeSpotify,
 }
 
 impl SpotifyClient {
     pub fn new() -> Self {
         let creds = Credentials::from_env().unwrap();
-        let client = ClientCredsSpotify::new(creds);
+        let oauth = OAuth::from_env(scopes!("user-follow-read user-follow-modify")).unwrap();
+        let client = AuthCodeSpotify::new(creds.clone(), oauth.clone());
 
         Self { client }
     }
 
     pub async fn authenticate(&self) {
-        self.client.request_token().await.unwrap();
+        let url = self.client.get_authorize_url(false).unwrap();
+        self.client
+            .prompt_for_token(url.as_str())
+            .await
+            .expect("Couldn't authenticate user")
+    }
+
+    pub async fn get_my_playlists(&self) {
+        self.client.token.lock().await.unwrap();
+        let limit: u32 = 50;
+        let offset: u32 = 0;
+        let playlists = self.client.current_user_playlists_manual(Some(limit), Some(offset)).await.unwrap();
+        println!("Playlists: {}", playlists.items.iter().map(|playlist| playlist.name.clone()).collect::<Vec<String>>().join(", "));
     }
 
     pub async fn get_album(&self, album_id: &str) -> Result<entities::Album> {
